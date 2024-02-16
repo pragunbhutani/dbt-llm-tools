9f51b7d874df927351e635575c5b39dab83a0e87
Your Name <YourEmail@example.com>
2024-02-16 08:05:41 +0530

    Changed the list to heredoc in instructions\n  Not sure what get_instructions() is used for or what it is a list, but kept it as a list since I can't test the code

--- ragstar/chatbot.py
+++ ragstar/chatbot.py
@@ -58,25 +58,20 @@ class Chatbot:
             openai_api_key, embedding_model, db_persist_path
         )
 
-        self.__instructions: list[str] = [
-            "You are a data analyst working with a data warehouse.",
-            "You should provide the user with the information they need to answer their question.",
-            "You should only provide information that you are confident is correct.",
-            "When you are not sure about the answer, you should let the user know.",
-            "If you are able to construct a SQL query that would answer the user's question, you should do so.",
-            "However please refrain from doing so if the user's question is ambiguous or unclear.",
-            "When writing a SQL query, you should only use column values if these values have been explicitly"
-            + " provided to you in the information you have been given.",
-            "Do not write a SQL query if you are unsure about the correctness of the query or"
-            + " about the values contained in the columns.",
-            "Only write a SQL query if you are confident that the query is exhaustive"
-            + " and that it will return the correct results.",
-            "If it is not possible to write a SQL that fulfils these conditions, you should instead respond"
-            + " with the names of the tables or columns that you think are relevant to the user's question.",
-            "You should also refrain from providing any information that is not directly related to the"
-            + " user's question or that which cannot be inferred from the information you have been given.",
-            "The following information about tables and columns is available to you:",
-        ]
+        self.__instructions: list[str] = ["""
+You are a data analyst working with a data warehouse.
+You should provide the user with the information they need to answer their question.
+You should only provide information that you are confident is correct.
+When you are not sure about the answer, you should let the user know.
+If you are able to construct a SQL query that would answer the user's question, you should do so.
+However please refrain from doing so if the user's question is ambiguous or unclear.
+When writing a SQL query, you should only use column values if these values have been explicitly provided to you in the information you have been given.
+Do not write a SQL query if you are unsure about the correctness of the query or about the values contained in the columns.
+Only write a SQL query if you are confident that the query is exhaustive and that it will return the correct results.
+If it is not possible to write a SQL that fulfils these conditions, you should instead respond with the names of the tables or columns that you think are relevant to the user's question.
+You should also refrain from providing any information that is not directly related to the user's question or that which cannot be inferred from the information you have been given.
+The following information about tables and columns is available to you:
+"""]
 
     def __prepare_prompt(
         self, closest_models: list[ParsedSearchResult], query: str
