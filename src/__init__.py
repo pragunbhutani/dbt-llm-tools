2516f44fb94c90f1824948054eae6e40b712e9d1
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2024-04-02 15:49:23 +0530

    added jupyter

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,11 @@ from src.chatbot import Chatbot
 from src.dbt_model import DbtModel
 from src.dbt_project import DbtProject
 from src.documentation_generator import DocumentationGenerator
-from src.instructions import (ANSWER_QUESTION_INSTRUCTIONS,
-                              INTERPRET_MODEL_INSTRUCTIONS)
-from src.types import (DbtModelDict, DbtModelDirectoryEntry,
-                       ParsedSearchResult, PromptMessage)
+from src.instructions import ANSWER_QUESTION_INSTRUCTIONS, INTERPRET_MODEL_INSTRUCTIONS
+from src.types import (
+    DbtModelDict,
+    DbtModelDirectoryEntry,
+    ParsedSearchResult,
+    PromptMessage,
+)
 from src.vector_store import VectorStore
